70684f5f55b072477c7756a117f0edae385d7507
Gonzalo Rafuls <grafuls@redhat.com>
2018-06-18 16:34:14 +0200

    Fade to black +fixes
    Change-Id: I21f50f5576d207e4e6bf7894928e76d9e4b118ef

--- disruption_generator/parsers/experiment_parser.py
+++ disruption_generator/parsers/experiment_parser.py
@@ -14,9 +14,7 @@ class IParser(zope.interface.Interface):
     """
     Parser for experiments with disruptive actions definitions.
     """
-    yaml_path = zope.interface.Attribute(
-        "The path to the scenario yaml"
-    )
+    yaml_path = zope.interface.Attribute("The path to the scenario yaml")
 
     def parse(self):
         """
@@ -42,9 +40,9 @@ class ExperimentParser(object):
                 Listener: object with listener info
             """
 
-            re = element_listener['re']
-            log_file = element_listener['log']
-            host = element_listener['host']
+            re = element_listener["re"]
+            log_file = element_listener["log"]
+            host = element_listener["host"]
             _listener = Listener(re=re, log=log_file, target=host)
             return _listener
 
@@ -60,35 +58,42 @@ class ExperimentParser(object):
 
             _actions = []
             for trigger in element_trigger:
-                key = 'action'
+                key = "action"
                 _name = trigger[key][config.NAME_STR]
-                params = trigger[key]['params']
-                target_host = trigger[key]['target_host']
-                wait = trigger[key]['wait']
-                timeout = trigger[key]['timeout']
+                params = trigger[key]["params"]
+                target_host = trigger[key]["target_host"]
+                wait = trigger[key]["wait"]
+                timeout = trigger[key]["timeout"]
 
-                trigger = Action(name=_name, params=params, target_host=target_host, wait=wait, timeout=timeout)
+                trigger = Action(
+                    name=_name,
+                    params=params,
+                    target_host=target_host,
+                    wait=wait,
+                    timeout=timeout,
+                )
                 _actions.append(trigger)
             return _actions
 
         doc = None
         try:
-            with open(self.yaml_path, 'r') as f:
-                import ipdb;ipdb.set_trace()
+            with open(self.yaml_path, "r") as f:
                 data = f.read()
                 doc = yaml.safe_load(data)
         except IOError as e:
-            logging.error('Failed to open file {file_name}, Why: {err}'.format(
-                file_name=self.yaml_path, err=e
-            ))
+            logging.error(
+                "Failed to open file {file_name}, Why: {err}".format(
+                    file_name=self.yaml_path, err=e
+                )
+            )
 
         for disrupt_action in doc:
             name = disrupt_action[config.DISRUPT_ACTION_STR][0][config.NAME_STR]
             listener = _init_listener(
-                    disrupt_action[config.DISRUPT_ACTION_STR][0][config.LISTENER_STR]
+                disrupt_action[config.DISRUPT_ACTION_STR][0][config.LISTENER_STR]
             )
             actions = _init_actions(
-                    disrupt_action[config.DISRUPT_ACTION_STR][0][config.TRIGGER_STR]
+                disrupt_action[config.DISRUPT_ACTION_STR][0][config.TRIGGER_STR]
             )
 
             disruption = Disruption(name=name, listener=listener, actions=actions)
