8aac1b7cc797e6af68e898c39e57f936263d1d4b
Jan Zmeskal <janzmeskal@email.cz>
2018-07-11 10:09:35 +0200

    Splitting tests into distinct modules

---
+++ tests/test_cli.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import pytest
+
+from click.testing import CliRunner
+
+from disruption_generator import cli
+
+
+@pytest.fixture(scope='module')
+def runner():
+    return CliRunner()
+
+
+def test_help_message(runner):
+    help_result = runner.invoke(cli.main, args=['--help'])
+    assert help_result.exit_code == 0
+    assert "Show this message and exit." in help_result.output
